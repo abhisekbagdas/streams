f654f85179cbb38eaddf36324a537f92a5fd11a7
Abhisek Bagdas <abhisekbagdas@gmail.com>
2026-02-11 09:52:52 +0545

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import os
+from PIL import Image
+from rembg import remove
+
+print("ok")
+st.title("Remove Backgroung")
+
+upload_file = st.file_uploader("Upload your image")
+
+if upload_file:
+  img = Image.open(upload_file)
+  st.subheader("Orignal Image")
+  st.image(img, use_container_width = True)
+
+  if st.button("Remove Background"):
+    with st.spinner("Removing Background..."):
+      output = remove(img)
+    st.subheader("Background Removed")
